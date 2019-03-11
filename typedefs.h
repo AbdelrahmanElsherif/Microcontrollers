27a15111a4940608668eef266a4ad4271b501a88
ahmeddgamall <36276961+ahmeddgamall@users.noreply.github.com>
2019-03-11 20:32:39 +0200

    Update typedefs.h

--- typedefs.h
+++ typedefs.h
@@ -1,6 +1,13 @@
 #ifndef TYPEDEFS_H
 #define TYPEDEFS_H
 
+#define PORTA_INDEX  0
+#define PORTB_INDEX  1
+#define PORTC_INDEX  2
+#define PORTD_INDEX  3
+#define PORTE_INDEX  4
+#define PORTF_INDEX  5
+
 typedef  signed    char   int8;
 typedef  unsigned  char   uint8;
 typedef  signed    short  int16;
