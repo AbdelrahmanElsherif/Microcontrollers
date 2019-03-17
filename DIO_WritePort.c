0fd122619192de313ea283775b4df2ebb4e5f709
Shehab Hosny <36346614+Shehab7osny@users.noreply.github.com>
2019-03-17 15:54:21 +0200

    Update DIO_WritePort.c

--- DIO_WritePort.c
+++ DIO_WritePort.c
@@ -7,21 +7,21 @@ void DIO_WritePort(uint8 port_index, uint8 pins_mask, Dio_LevelType pins_level)
 {
   if(pins_level == STD_HIGH)
   {
-    if(port_index == PORTA_INDEX)		   GPIO_PORTA_DATA_R |= pins_mask;
-    else if(port_index == PORTB_INDEX)	GPIO_PORTB_DATA_R |= pins_mask;
-    else if(port_index == PORTC_INDEX)	GPIO_PORTC_DATA_R |= pins_mask;
-    else if(port_index == PORTD_INDEX)	GPIO_PORTD_DATA_R |= pins_mask;
-    else if(port_index == PORTE_INDEX)	GPIO_PORTE_DATA_R |= pins_mask;
-    else if(port_index == PORTF_INDEX)	GPIO_PORTF_DATA_R |= pins_mask;
+    if(port_index == PORTA_INDEX)		      GPIO_PORTA_DATA_R |= pins_mask;
+    else if(port_index == PORTB_INDEX)	  GPIO_PORTB_DATA_R |= pins_mask;
+    else if(port_index == PORTC_INDEX)	  GPIO_PORTC_DATA_R |= pins_mask;
+    else if(port_index == PORTD_INDEX)	  GPIO_PORTD_DATA_R |= pins_mask;
+    else if(port_index == PORTE_INDEX)	  GPIO_PORTE_DATA_R |= pins_mask;
+    else if(port_index == PORTF_INDEX)	  GPIO_PORTF_DATA_R |= pins_mask;
   }
   
   else if(pins_level == STD_LOW)
   {
-    if(port_index == PORTA_INDEX)		   GPIO_PORTA_DATA_R &= ~pins_mask;
-    else if(port_index == PORTB_INDEX)	GPIO_PORTB_DATA_R &= ~pins_mask;
-    else if(port_index == PORTC_INDEX)	GPIO_PORTC_DATA_R &= ~pins_mask;
-    else if(port_index == PORTD_INDEX)	GPIO_PORTD_DATA_R &= ~pins_mask;
-    else if(port_index == PORTE_INDEX)	GPIO_PORTE_DATA_R &= ~pins_mask;
-    else if(port_index == PORTF_INDEX)	GPIO_PORTF_DATA_R &= ~pins_mask;
+    if(port_index == PORTA_INDEX)		      GPIO_PORTA_DATA_R &= ~pins_mask;
+    else if(port_index == PORTB_INDEX)	  GPIO_PORTB_DATA_R &= ~pins_mask;
+    else if(port_index == PORTC_INDEX)	  GPIO_PORTC_DATA_R &= ~pins_mask;
+    else if(port_index == PORTD_INDEX)	  GPIO_PORTD_DATA_R &= ~pins_mask;
+    else if(port_index == PORTE_INDEX)	  GPIO_PORTE_DATA_R &= ~pins_mask;
+    else if(port_index == PORTF_INDEX)	  GPIO_PORTF_DATA_R &= ~pins_mask;
   }
 }
