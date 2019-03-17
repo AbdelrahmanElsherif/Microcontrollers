354e294f67bcb8684014476287effffcc8f1c0f3
ahmeddgamall <36276961+ahmeddgamall@users.noreply.github.com>
2019-03-17 20:24:50 +0200

    Update port_init.c

--- port_init.c
+++ port_init.c
@@ -5,35 +5,55 @@
 
 void Port_Init(uint8 port_index)
 {
-	SYSCTL->RCGCGPIO |= 0x01 << port_index;
+	uint8 delay;
+	
+	SYSCTL_RCGCGPIO_R |= 0x01 << port_index;
+	
+	delay = 1;
+	delay = 2;
+	delay = 3;
+	delay = 4;
+	
 	if(port_index == PORTA_INDEX)
 	{
-		GPIOA->LOCK |= 0x4C4F434B;
-		GPIOA->DEN |= 0xFF;
+		GPIO_PORTA_LOCK_R |= 0x4C4F434B;
+		GPIO_PORTA_CR_R |= 0xFF;
+		GPIO_PORTA_LOCK_R = 0;
+		GPIO_PORTA_DEN_R |= 0xFF;
 	}
 	else if(port_index == PORTB_INDEX)
 	{
-		GPIOB->LOCK |= 0x4C4F434B;
-		GPIOB->DEN |= 0xFF;
+		GPIO_PORTB_LOCK_R |= 0x4C4F434B;
+		GPIO_PORTB_CR_R |= 0xFF;
+		GPIO_PORTB_LOCK_R = 0;
+		GPIO_PORTB_DEN_R |= 0xFF;
 	}
 	else if(port_index == PORTC_INDEX)
 	{
-		GPIOC->LOCK |= 0x4C4F434B;
-		GPIOC->DEN |= 0xF0;
+		GPIO_PORTC_LOCK_R |= 0x4C4F434B;
+		GPIO_PORTC_CR_R |= 0xF0;
+		GPIO_PORTC_LOCK_R = 0;
+		GPIO_PORTC_DEN_R |= 0xF0;
 	}
 	else if(port_index == PORTD_INDEX)
 	{
-		GPIOD->LOCK |= 0x4C4F434B;
-		GPIOD->DEN |= 0xFF;
+		GPIO_PORTD_LOCK_R |= 0x4C4F434B;
+		GPIO_PORTD_CR_R |= 0xFF;
+		GPIO_PORTD_LOCK_R = 0;
+		GPIO_PORTD_DEN_R |= 0xFF;
 	}
 	else if(port_index == PORTE_INDEX)
 	{
-		GPIOE->LOCK |= 0x4C4F434B;
-		GPIOE->DEN |= 0x3F;
+		GPIO_PORTE_LOCK_R |= 0x4C4F434B;
+		GPIO_PORTE_CR_R |= 0x3F;
+		GPIO_PORTE_LOCK_R = 0;
+		GPIO_PORTE_DEN_R |= 0x3F;
 	}
 	else if(port_index == PORTF_INDEX)
 	{
-		GPIOF->LOCK |= 0x4C4F434B;
-		GPIOF->DEN |= 0x1F;
+		GPIO_PORTF_LOCK_R |= 0x4C4F434B;
+		GPIO_PORTF_CR_R |= 0x1F;
+		GPIO_PORTF_LOCK_R = 0;
+		GPIO_PORTF_DEN_R |= 0x1F;
 	}
 }
